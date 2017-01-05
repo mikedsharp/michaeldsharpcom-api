e35f0c926d4109a2f85522dcac1407585a9dcef5
Mike Sharp <msharp23@hotmail.co.uk>
2017-01-05 17:48:59 +0000

    implemented google drive calls for feedback

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var GoogleSpreadsheet = require('google-spreadsheet');
+var async = require('async');
 
 /* POST likes/dislikes. */
 router.post('/', function(req, res, next) {
@@ -9,6 +11,51 @@ router.post('/', function(req, res, next) {
       console.log('dislike registered');
   }
   res.json({message: 'like (or dislike recorded!)'});
+
+  var doc = new GoogleSpreadsheet('1kS7juEJ9G4uxKSplOf5HnWsWC3f6F66moVzKMeTFqxI');
+  var sheet;
+
+  async.series([
+  function setAuth(step) {
+    // see notes below for authentication instructions!
+    var creds_json = {
+      client_email: process.env.GOOGLE_DRIVE_EMAIL,
+      private_key:  process.env.GOOGLE_PRIVATE_KEY
+    }
+    doc.useServiceAccountAuth(creds_json, step);
+  },
+  function getInfoAndWorksheets(step) {
+    doc.getInfo(function(err, info) {
+      console.log('Loaded doc: '+info.title+' by '+info.author.email);
+      sheet = info.worksheets[0];
+      console.log('sheet 1: '+sheet.title+' '+sheet.rowCount+'x'+sheet.colCount);
+      step();
+    });
+  },
+  function workingWithCells(step) {
+    sheet.getCells({
+      'min-row': 2,
+      'max-row': 4,
+      'return-empty': true
+    }, function(err, cells) {
+
+      var cellLike = cells[1];
+      var lastLike = cells[2]; 
+      var cellDislike = cells[27];
+      var lastDislike = cells[28]; 
+
+      if(req.body.like === 'true'){
+        cellLike.value = parseInt(cellLike.value) + 1; 
+        lastLike.value = new Date(Date.now()).toString(); 
+      } else {
+        cellDislike.value = parseInt(cellDislike.value) + 1; 
+        lastDislike.value = new Date(Date.now()).toString(); 
+      }
+      sheet.bulkUpdateCells(cells, step); //async
+    });
+  }
+]);
+
 });
 
 module.exports = router;
